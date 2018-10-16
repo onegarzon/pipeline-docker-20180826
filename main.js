cafeb522254f7766684e052f05e225c8b1c0b5b8
Juan Garzon <jg69mail@gmail.com>
2018-10-16 18:42:37 -0400

    added index.html

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // load the http module
 var http = require('http');
+var fs = require('fs');
 
 //
 //create a server object:
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World----->>>>!'); //write a response to the client
-  res.end(); //end the response
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var myReadStream = fs.createReadStream('index.html', 'utf8');
+  myReadStream.pipe(res);
 }).listen(8089); //the server object listens on port 8089
