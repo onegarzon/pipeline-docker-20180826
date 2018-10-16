c0a2e96f46488011c07c82ac617dfc29d68c2c9b
Juan Garzon <jg69mail@gmail.com>
2018-10-15 22:15:37 -0400

    update to main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Miiro. Thanks so much for sharing at https://getintodevops.com/blog/building-your-first-docker-image-with-jenkins-2-guide-for-developers. This is Juan\n");
+  response.end("Hello Miiro. Thanks so much....for sharing at https://getintodevops.com/blog/building-your-first-docker-image-with-jenkins-2-guide-for-developers. This is Juan\n");
 });
 
 // listen on localhost:8000
