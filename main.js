b88974f6659299983c3cf41a066719090a0f5bd2
Juan Garzon <jg69mail@gmail.com>
2018-10-15 23:50:58 -0400

    changed main.js and Dockerfile port 8089

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var http = require('http');
 //
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
+  res.write('Hello World----->>>>!'); //write a response to the client
   res.end(); //end the response
 }).listen(8089); //the server object listens on port 8089
